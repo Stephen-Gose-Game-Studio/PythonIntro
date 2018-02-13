41065966a31a3b1d47a5135464510b90a1fd0f2a
Brandon Nay <branay@uat.edu>
2018-02-13 00:00:04 -0700

    Added W13

---
+++ W13 Zoo Part 1/Nay_Brandon_Zoo_Part_1.py
@@ -0,0 +1,18 @@
+################################################################################
+# Brandon Nay
+# Zoo Part 1
+################################################################################
+
+
+class Animal:
+    def __init__(self, animal_type, age, color, legs):
+        self.animal_type = ''
+        self.age = 0
+        self.color = ''
+        self.legs = 0
+        
+    def __str__(self):
+        print(self.animal_type)
+        print(self.age)
+        print(self.color)
+        print(self.legs)
