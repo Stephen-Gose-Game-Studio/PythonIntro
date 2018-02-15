975621bbf50ceedd8f3ae21e0204f10079dbb5bb
Brandon Nay <branay@uat.edu>
2018-02-15 12:35:54 -0700

    Added Comments

--- W13 Zoo Part 1/Nay_Brandon_Zoo_Part_1.py
+++ W13 Zoo Part 1/Nay_Brandon_Zoo_Part_1.py
@@ -19,11 +19,12 @@
 
 class Animal:
     def __init__(self, animal_type, age, color, legs):
-        self.animal_type = animal_type
-        self.age = age
-        self.color = color
-        self.legs = legs
+        self.animal_type = animal_type  # str
+        self.age = age                  # int
+        self.color = color              # str
+        self.legs = legs                # int
 
+    # Override class method to print something readable instead of memory loc
     def __str__(self):
         return "\nANIMAL:    {}\n" \
                "AGE:       {}\n" \
@@ -76,6 +77,7 @@ class Insect(Animal):
 # attributes associated with each.
 ###############################################################################
 
+# Initialize 2 instances of each subclass into a list
 zoo = [Reptile('Snake', 2, 'Orange', 0, True),
        Reptile('Gecko', 12, 'Brown', 4, False),
        Mammal('Bear', 7, 'Black', 4, True),
@@ -83,5 +85,6 @@ zoo = [Reptile('Snake', 2, 'Orange', 0, True),
        Insect('Spider', 1, 'Black', 8, 0),
        Insect('Dragonfly', 1, 'Green', 6, 4)]
 
+# call __str__ for each object in zoo[]
 for x in zoo:
     print(x)
